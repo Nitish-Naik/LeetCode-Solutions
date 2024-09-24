1ae1d797a42614da773ff6d395d8c30c022c05f7
Nitish-Naik <126327334+Nitish-Naik@users.noreply.github.com>
2024-09-24 06:43:08 +0530

    24 09 2024

---
+++ LeetCode_3043.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+
+class LeetCode_3043 {
+    public static int longestCommonPrefix(int[] arr1, int[] arr2) {
+        HashMap<String, Integer> prefixMap = new HashMap<>();
+        
+        // Step 1: Build the prefix map for arr1
+        for (int num : arr1) {
+            String strNum = Integer.toString(num);
+            String prefix = "";
+            for (char ch : strNum.toCharArray()) {
+                prefix += ch;
+                prefixMap.put(prefix, prefixMap.getOrDefault(prefix, 0) + 1);
+            }
+        }
+        
+        int maxLength = 0;
+        
+        // Step 2: Check for common prefixes in arr2
+        for (int num : arr2) {
+            String strNum = Integer.toString(num);
+            String prefix = "";
+            for (char ch : strNum.toCharArray()) {
+                prefix += ch;
+                if (prefixMap.containsKey(prefix)) {
+                    maxLength = Math.max(maxLength, prefix.length());
+                }
+            }
+        }
+        
+        return maxLength;
+    }
+
+    public static void main(String args[]) {
+        int a[] = {1, 100, 10}, b[] = {1000};
+        System.out.println(longestCommonPrefix(a, b));
+    }
+}
