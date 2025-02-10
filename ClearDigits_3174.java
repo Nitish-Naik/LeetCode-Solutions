29882981d34cd89b3b9948fe66c6f63317145daa
P Nitish <nitishnaik2022@gmail.com>
2025-02-10 19:44:47 +0530

    Create ClearDigits_3174.java

---
+++ ClearDigits_3174.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String clearDigits(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()) {
+            if(Character.isDigit(c)) {
+                stack.pop();
+            }
+            else {
+                stack.push(c);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(char c : stack) {
+            sb.append(c);
+        }
+
+        return sb.toString();
+    }
+}
