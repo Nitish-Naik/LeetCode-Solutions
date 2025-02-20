f6ec34b593409d5ced497e243b359691e5cc5cbd
P Nitish <nitishnaik2022@gmail.com>
2025-02-20 06:49:36 +0000

    commit

---
+++ FEBRUARY/Find_Unique_Binary_String_1980.java
@@ -0,0 +1,17 @@
+public class Find_Unique_Binary_String_1980 {
+
+    public String findDifferentBinaryString(String[] nums) {
+        int n=nums.length;
+        StringBuilder sb=new StringBuilder();
+
+        for(int i=0; i<n; i++){   
+            if(nums[i].charAt(i)=='0'){         
+                sb.append('1');
+            }else{
+                sb.append('0');
+            }            
+        }
+        
+        return sb.toString();
+    }
+}
